d89ff2476b86a4337996172764ecc489899fa8ac
skarlett <skarlett@github.com>
2025-02-07 22:41:35 -0600

    add git-hooks

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     lynx.url = "github:the-computer-club/lynx";
     asluni.url = "github:the-computer-club/automous-zones";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
 
     disko = {
       url = "github:nix-community/disko";
@@ -12,29 +13,51 @@
   };
 
   outputs = inputs @ { self, nixpkgs, flake-parts, ... }:
-  flake-parts.lib.mkFlake { inherit inputs; }
-  ({ ... }: let
-    inherit (nixpkgs) lib;
-    fileAttrs = lib.filterAttrs (_: t: t == "regular" ) (builtins.readDir ./src);
-    fileList = with builtins; filter (lib.hasSuffix "nix") (attrNames fileAttrs);
-    src = map (file: ./src/${file}) fileList;
-  in {
-    imports = with inputs; [
-      lynx.flakeModules.builtins
-      lynx.flakeModules.flake-guard
-      asluni.flakeModules.asluni
-    ];
-
-    config = {
-      systems = [ "x86_64-linux" ];
-      flake = {
-        nixosConfigurations.helium = lib.nixosSystem {
-          specialArgs = { inherit inputs self; };
-          modules = src ++ [
-            inputs.disko.nixosModules.disko
+    flake-parts.lib.mkFlake { inherit inputs; }
+      ({ ... }:
+        let
+          inherit (nixpkgs) lib;
+          fileAttrs = lib.filterAttrs (_: t: t == "regular") (builtins.readDir ./src);
+          fileList = with builtins; filter (lib.hasSuffix "nix") (attrNames fileAttrs);
+          src = map (file: ./src/${file}) fileList;
+        in
+        {
+          imports = with inputs; [
+            lynx.flakeModules.builtins
+            lynx.flakeModules.flake-guard
+            asluni.flakeModules.asluni
+            git-hooks-nix.flakeModule
           ];
-        };
-      };
-    };
-  });
+
+          config = {
+            systems = [ "x86_64-linux" ];
+
+            perSystem = { config, pkgs, ... }: {
+              pre-commit.check.enable = true;
+
+              pre-commit.settings.hooks = {
+                trim-trailing-whitespace.enable = true;
+                nixpkgs-fmt.enable = true;
+              };
+
+              devShells.default = pkgs.mkShell {
+                shellHook = lib.concatStringsSep "\n" [
+                  config.pre-commit.installationScript
+                ];
+                packages = with pkgs; [
+                  pre-commit
+                ];
+              };
+            };
+
+            flake = {
+              nixosConfigurations.helium = lib.nixosSystem {
+                specialArgs = { inherit inputs self; };
+                modules = src ++ [
+                  inputs.disko.nixosModules.disko
+                ];
+              };
+            };
+          };
+        });
 }
