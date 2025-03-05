f8cf2e4b494c1b91accbc19000f84dcf3657b9eb
Jermeiah S <owner@arouzing.xyz>
2025-03-05 13:32:46 -0500

    feat: add soft-serve module
    Update src/soft-serve.nix
    
    change option name
    
    Co-authored-by: SkettiSouls <146829563+SkettiSouls@users.noreply.github.com>
    
    Update src/soft-serve.nix
    
    fix access to users keys
    
    Co-authored-by: SkettiSouls <146829563+SkettiSouls@users.noreply.github.com>
    
    Update src/soft-serve.nix
    
    inherit lib
    
    Co-authored-by: SkettiSouls <146829563+SkettiSouls@users.noreply.github.com>
    
    Update src/soft-serve.nix
    
    Co-authored-by: SkettiSouls <146829563+SkettiSouls@users.noreply.github.com>

---
+++ src/soft-serve.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  inherit (lib)
+    attrValues
+    flatten
+    mapAttrs
+    ;
+
+  cfg = config.helion.soft-serve;
+in
+{
+  options.helion.soft-serve = {
+    enable = lib.mkEnableOption "";
+    admins = lib.mkOption {
+      type = with lib.types; listOf str;
+      default = flatten (attrValues (mapAttrs (_: ucfg: ucfg.sshKeys) config.helion.users));
+    };
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 9999;
+    };
+    domain = lib.mkOption {
+      default = "git.helium.luni";
+      type = lib.types.str;
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    # This fix was recently added to upstream but is not in repo yet
+    systemd.services.soft-serve.restartTriggers = [
+      ((pkgs.formats.yaml { }).generate "config.yaml" config.services.soft-serve.settings)
+    ];
+
+    services.soft-serve = {
+      enable = true;
+      settings = {
+        name = "luni repos";
+        log_format = "text";
+        ssh = {
+          listen_addr = ":${builtins.toString cfg.port}";
+          public_url = "ssh://${cfg.domain}:${builtins.toString cfg.port}";
+        };
+        stats.listen_addr = ":23235";
+        lfs.enabled = true;
+        initial_admin_keys = cfg.admins;
+      };
+    };
+
+  };
+}
