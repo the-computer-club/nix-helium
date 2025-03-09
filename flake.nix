7ec2e72cb58cf625e51f3d81639db341cd03d22f
Skarlett <skarlett@github.com>
2025-03-09 20:22:13 +1100

    run `nix flake check` before commits
    (cherry picked from commit 4378d7005a6408abc1c2a97bd502db7efd6bcb94)

--- flake.nix
+++ flake.nix
@@ -12,29 +12,61 @@
   };
 
   outputs = inputs @ { self, nixpkgs, flake-parts, ... }:
-  flake-parts.lib.mkFlake { inherit inputs; }
-  ({ ... }: let
-    inherit (nixpkgs) lib;
-    fileAttrs = lib.filterAttrs (_: t: t == "regular" ) (builtins.readDir ./src);
-    fileList = with builtins; filter (lib.hasSuffix "nix") (attrNames fileAttrs);
-    src = map (file: ./src/${file}) fileList;
-  in {
-    imports = with inputs; [
-      lynx.flakeModules.builtins
-      lynx.flakeModules.flake-guard
-      asluni.flakeModules.asluni
-    ];
-
-    config = {
-      systems = [ "x86_64-linux" ];
-      flake = {
-        nixosConfigurations.helium = lib.nixosSystem {
-          specialArgs = { inherit inputs self; };
-          modules = src ++ [
-            inputs.disko.nixosModules.disko
+    flake-parts.lib.mkFlake { inherit inputs; }
+      ({ ... }:
+        let
+          inherit (nixpkgs) lib;
+          fileAttrs = lib.filterAttrs (_: t: t == "regular") (builtins.readDir ./src);
+          fileList = with builtins; filter (lib.hasSuffix "nix") (attrNames fileAttrs);
+          src = map (file: ./src/${file}) fileList;
+        in
+        {
+          imports = with inputs; [
+            lynx.flakeModules.builtins
+            lynx.flakeModules.flake-guard
+            asluni.flakeModules.asluni
           ];
-        };
-      };
-    };
-  });
-}
+
+          config = {
+            systems = [ "x86_64-linux" ];
+            flake = {
+              nixosConfigurations.helium = lib.nixosSystem
+                {
+                  specialArgs = { inherit inputs self; };
+                  modules = src ++ [
+                    inputs.disko.nixosModules.disko
+                  ];
+                  config = {
+                    systems = [ "x86_64-linux" ];
+
+                    perSystem = { config, pkgs, ... }: {
+                      pre-commit.check.enable = true;
+
+                      pre-commit.settings.hooks = {
+                        trim-trailing-whitespace.enable = true;
+                        nixpkgs-fmt.enable = true;
+                        flake-checker.enable = true;
+                      };
+
+                      devShells.default = pkgs.mkShell {
+                        shellHook = lib.concatStringsSep "\n" [
+                          config.pre-commit.installationScript
+                        ];
+                        packages = with pkgs; [
+                          pre-commit
+                        ];
+                      };
+                    };
+
+                    flake = {
+                      nixosConfigurations.helium = lib.nixosSystem {
+                        specialArgs = { inherit inputs self; };
+                        modules = src ++ [
+                          inputs.disko.nixosModules.disko
+                          inputs.lynx.nixosModules.flake-guard-host
+                        ];
+                      };
+                    };
+                  };
+                });
+            }
